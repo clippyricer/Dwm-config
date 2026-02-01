59da1cd4c43952991bf5576e8855c5361d5d8db4
clippyricer <bannansmooth68@outlook.com>
2026-02-01 15:30:12 -0500

    get slstatus bar

--- slstatus-1.1/config.h
+++ slstatus-1.1/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 500;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -66,5 +66,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+  { battery_perc, "[  %s%]", "BAT0" },
+  { battery_remaining, "[󱧥 %s]", "BAT0" },
+  { wifi_essid, "[  %s]",    "wlan0" },
+  { wifi_perc, "[Signal %s%]", "wlan0" },
+	{ datetime, "[ %s]",      "%F %T" },
+ 
 };
